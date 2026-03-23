845d83e6eed4cca62cdbfef080e5b299f335cbaf
Stewart <zhiwei3217@gmail.com>
2026-03-23 18:24:24 +0800

    Remove Mutant Core project files and directories

---
+++ Packages/com.mutant.core/Runtime/Bootstrap/ModuleBootstrap.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace Bill.Mutant.Core
+{
+	public static class ModuleBootstrap
+	{
+		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+		private static void Initialize()
+		{
+			var logConfig = Resources.Load<LogConfig>("Mutant/LogConfig");
+			if (logConfig == null)
+			{
+				logConfig = ScriptableObject.CreateInstance<LogConfig>();
+			}
+
+			Logger.Init(logConfig);
+			UnityLogBridge.Install();
+
+			Logger.Info(LogCategory.Core, "Mutant Bootstrap Start", nameof(ModuleBootstrap));
+
+			ModuleManager.Instance.Init();
+
+			var go = new GameObject("[Mutant.Driver]");
+			Object.DontDestroyOnLoad(go);
+			go.hideFlags = HideFlags.HideInHierarchy;
+			go.AddComponent<ModuleDriver>();
+		}
+	}
+}
