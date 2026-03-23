362d9b9578eb86bb4d46fce52dac262dac499f20
Stewart <zhiwei3217@gmail.com>
2026-03-23 17:30:03 +0800

    Merge pull request #16 from BillStewartzhiwei/feature/log-module
    Feature/log moduleAdd meta files for new and updated project structure and rename package to com.mutant.core

--- Packages/com.billstewart.core/Runtime/Pattern/MonoSingleton.cs
+++ Packages/com.billstewart.core/Runtime/Pattern/MonoSingleton.cs
@@ -2,51 +2,51 @@
 {
     using UnityEngine;
 
-public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
-{
-    private static T _instance;
-    private static bool _isQuitting = false;
+	public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+	{
+		private static T _instance;
+		private static bool _isQuitting = false;
 
-    public static T Instance
-    {
-        get
-        {
-            if (_isQuitting) return null;
+		public static T Instance
+		{
+			get
+			{
+				if (_isQuitting) return null;
 
-            if (_instance == null)
-            {
-                _instance = FindObjectOfType<T>();
+				if (_instance == null)
+				{
+					_instance = FindObjectOfType<T>();
 
-                if (_instance == null)
-                {
-                    var go = new GameObject(typeof(T).Name);
-                    _instance = go.AddComponent<T>();
-                    DontDestroyOnLoad(go);
-                }
-            }
-            return _instance;
-        }
-    }
+					if (_instance == null)
+					{
+						var go = new GameObject(typeof(T).Name);
+						_instance = go.AddComponent<T>();
+						DontDestroyOnLoad(go);
+					}
+				}
+				return _instance;
+			}
+		}
 
-    protected virtual void Awake()
-    {
-        if (_instance == null)
-        {
-            _instance = this as T;
-            DontDestroyOnLoad(gameObject);
-            OnInit();
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
+		protected virtual void Awake()
+		{
+			if (_instance == null)
+			{
+				_instance = this as T;
+				DontDestroyOnLoad(gameObject);
+				OnInit();
+			}
+			else
+			{
+				Destroy(gameObject);
+			}
+		}
 
-    protected virtual void OnInit() { }
+		protected virtual void OnInit() { }
 
-    protected virtual void OnApplicationQuit()
-    {
-        _isQuitting = true;
-    }
-}
+		protected virtual void OnApplicationQuit()
+		{
+			_isQuitting = true;
+		}
+	}
 }
