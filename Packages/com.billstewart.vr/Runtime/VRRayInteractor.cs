905cace040341bddbb7c0b980049d0fe35d67c73
Bill Stewart <zhiwei3217@gmail.com>
2026-03-23 10:18:13 +0800

    Feature/vr module (#6)
    * feat(vr): add VRManager basic skeleton
    
    * feat(vr): add basic ray interactor

---
+++ Packages/com.billstewart.vr/Runtime/VRRayInteractor.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Bill.Mutant.VR
+{
+    public class VRRayInteractor : MonoBehaviour
+    {
+        public float Distance = 10f;
+        public LayerMask LayerMask;
+
+        void Update()
+        {
+            Ray ray = new Ray(transform.position, transform.forward);
+            if (Physics.Raycast(ray, out var hit, Distance, LayerMask))
+            {
+                Debug.DrawLine(ray.origin, hit.point, Color.green);
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    ExecuteEvents.Execute(hit.collider.gameObject,
+                        new PointerEventData(EventSystem.current),
+                        ExecuteEvents.pointerClickHandler);
+                }
+            }
+        }
+    }
