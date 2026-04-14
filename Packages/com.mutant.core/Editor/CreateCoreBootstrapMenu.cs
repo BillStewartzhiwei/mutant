446e13c8cc2d6cb3bac3a08d7af896a545c01447
Stewart <zhiwei3217@gmail.com>
2026-04-14 11:28:57 +0800

    移除 `CoreBootstrap` 类并更新相关文件
    - 删除 `CoreBootstrap` 类及其 meta 文件
    - 注释掉 `CreateCoreBootstrapMenu` 中的相关代码

--- Packages/com.mutant.core/Editor/CreateCoreBootstrapMenu.cs
+++ Packages/com.mutant.core/Editor/CreateCoreBootstrapMenu.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEditor;
 using UnityEngine;
-using Mutant.Core.Bootstrap;
+// using Mutant.Core.Bootstrap;
 
 namespace Mutant.Core.Editor
 {
@@ -9,14 +9,14 @@ namespace Mutant.Core.Editor
 		[MenuItem("GameObject/Mutant/Create Core Bootstrap", false, 10)]
 		public static void Create()
 		{
-			GameObject go = new GameObject("[MutantCore]");
-			go.AddComponent<CoreBootstrap>();
-
-			if (Selection.activeTransform != null)
-				go.transform.SetParent(Selection.activeTransform, false);
-
-			Undo.RegisterCreatedObjectUndo(go, "Create Core Bootstrap");
-			Selection.activeGameObject = go;
+			// GameObject go = new GameObject("[MutantCore]");
+			// go.AddComponent<CoreBootstrap>();
+			//
+			// if (Selection.activeTransform != null)
+			// 	go.transform.SetParent(Selection.activeTransform, false);
+			//
+			// Undo.RegisterCreatedObjectUndo(go, "Create Core Bootstrap");
+			// Selection.activeGameObject = go;
 		}
 	}
 }
